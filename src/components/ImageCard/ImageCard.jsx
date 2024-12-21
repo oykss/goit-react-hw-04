fb6c74a6a70a488404f993d2cb49f0367edb1964
Pavlo-Chernicenko <pwned7067@gmail.com>
2024-12-21 19:37:24 +0200

    Fixed ImageCard component

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,9 +1,13 @@
 import css from './ImageCard.module.css';
 
-export default function ImageCard({ linkImage, alt }) {
+export default function ImageCard({
+  urls: { small, regular },
+  alt,
+  handleIsOpen,
+}) {
   return (
     <div className={css.wrap}>
-      <img src={linkImage} alt={alt} />
+      <img src={small} alt={alt} onClick={() => handleIsOpen(regular)} />
     </div>
   );
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -5,8 +5,12 @@ export default function ImageGallery({ photos, handleIsOpen }) {
   return (
     <ul className={css.list}>
       {photos.map(({ urls: { small, regular }, alt_description }, id) => (
-        <li className={css.item} key={id} onClick={() => handleIsOpen(regular)}>
-          <ImageCard linkImage={small} alt={alt_description} />
+        <li className={css.item} key={id}>
+          <ImageCard
+            urls={{ small, regular }}
+            alt={alt_description}
+            handleIsOpen={handleIsOpen}
+          />
         </li>
       ))}
     </ul>
